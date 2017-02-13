807be26576e8cedf26f11aed363b32890ffe6b3c
Ray-E-Artz <ray.e.artz@gmail.com>
2017-02-13 15:58:56 -0600

    rewrote to not use "repeat" method for string

--- index.ts
+++ index.ts
@@ -53,6 +53,45 @@ export namespace Tree {
   }
 }
 
+export namespace NameList {
+
+  export function label(names: string[]) : string {
+
+    if (names.length == 0) return '';
+
+    let prefixItem = '- ';
+    let midfixItem = '& ';
+//    let prefix = prefixItem.repeat(Math.max(0, names.length - 2 ));
+    let prefix = '';
+    for (let i=0; i < names.length - 2; i++){
+      prefix += prefixItem;
+    }
+
+    let midfix = '';
+    if (names.length > 1) {
+      midfix = midfixItem;
+    }
+    return prefix + midfix + names[names.length - 1];
+  }
+
+  export function displayName(prefix: string, names: string[]) : string {
+
+    let prefixItem = ': ';
+    let midfixItem = ' & ';
+    let displayName = prefix;
+
+    if (names.length == 0) return displayName;
+
+    displayName = displayName + prefixItem + names[0];
+
+    for (let i=1; i < names.length; i++) {
+      displayName = displayName + midfixItem + names[i];
+    }
+    return displayName;
+  }
+
+}
+
 /* remaining code for testing */
 
 let aTree =
@@ -95,39 +134,7 @@ console.log(JSON.stringify(aTree, null, 2));
 console.log('');
 
 
-export namespace NameList {
 
-  export function label(names: string[]) : string {
-
-    if (names.length == 0) return '';
-
-    let prefixItem = '- ';
-    let midfixItem = '& ';
-    let prefix = prefixItem.repeat(Math.max(0, names.length - 2 ));
-    let midfix = '';
-    if (names.length > 1) {
-      midfix = midfixItem;
-    }
-    return prefix + midfix + names[names.length - 1];
-  }
-
-  export function displayName(prefix: string, names: string[]) : string {
-
-    let prefixItem = ': ';
-    let midfixItem = ' & ';
-    let displayName = prefix;
-
-    if (names.length == 0) return displayName;
-
-    displayName = displayName + prefixItem + names[0];
-
-    for (let i=1; i < names.length; i++) {
-      displayName = displayName + midfixItem + names[i];
-    }
-    return displayName;
-  }
-
-}
 
 let values = Tree.parseForValues(aTree);
 
